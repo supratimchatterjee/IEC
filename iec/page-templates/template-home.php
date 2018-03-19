61197595e25efd22cf348584b1623be2791674c2
supratimchatterjee <36877257+supratimchatterjee@users.noreply.github.com>
2018-03-19 13:05:04 +0530

    IEC
    Final Commit

---
+++ iec/page-templates/template-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying home page
+ * Template Name: Home
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package IEC
+ */
+?>
+	
+<?php get_header(); ?>
+
+
+<?php get_footer(); ?>
